522761706966151a7010d24bac2d1afaa7422a81
Kaif76079 <mdkaifcare@gmail.com>
2025-10-19 11:28:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,9 @@ class Config(object):
     # get a token from @BotFather
     pass
     """
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = int(os.environ["API_ID", ]
-    API_HASH = os.environ["API_HASH", ""]
-    AUTH_USERS = """"
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8159648223:AAFP3Cxj863pdJuPkmjdJDmEA2ud65RYJUY")
+    API_ID = int(os.environ["API_ID", "22471119"]
+    API_HASH = os.environ["API_HASH", "b8ab98fab8020ad7eab4c38f1ec9e1ae"]
+    AUTH_USERS = [""""]
  
+
